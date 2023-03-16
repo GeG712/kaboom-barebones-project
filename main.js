7ba5fe9e5bd9d30503eeec0c51aa94679d808692
ATallSteve <atallsteve@gmail.com>
2023-03-16 13:57:47 +0100

    a

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ kaboom()
 loadSprite("mole", "assets/mole.jpg");
 loadSprite("bg", "assets/download.jpeg");
 
+scene("start", () => {
+  add([pos(0, 0), color(0, 0, 255), rect(width(), height())]);
+
+  add([
+    "startGame",
+    text("Click here to start", { size: 60 }),
+    pos(width() / 2 - 400, height() / 2 - 100),
+    area(),
+  ]);
+  onClick("startGame", () => {
+    go("game");
+  });
+});
+
 kaboom({
   width: 320,
   height: 240,
